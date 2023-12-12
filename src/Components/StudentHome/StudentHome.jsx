cf69fa9e1925ad5ad9d19421ac06967ed8e4ccef
shamil <shamilmohd418@gmail.com>
2023-12-12 20:14:14 +0530

    jhg

--- src/Components/StudentHome/StudentHome.jsx
+++ src/Components/StudentHome/StudentHome.jsx
@@ -24,28 +24,28 @@ const StudentHome = () => {
   }, [studentid]);
 
 
-  const GetName = () => {
-    const key = localStorage.key(0);
-    const value = JSON.parse(localStorage.getItem(key));
+  // const GetName = () => {
+  //   const key = localStorage.key(0);
+  //   const value = JSON.parse(localStorage.getItem(key));
 
-    axios.get('http://localhost:3041/college/home', {
-      headers: {
-        Authorization: `Bearer ${value}`,
-      },
-    })
-      .then((response) => {
-        const { data } = response;
-        const { msg } = data;
-        document.getElementById("name").innerHTML = msg ? `${msg}` : "No message available";
-      })
-      .catch((error) => {
-        console.error("Axios error:", error);
-      });
-  };
+  //   axios.get('http://localhost:3041/college/home', {
+  //     headers: {
+  //       Authorization: `Bearer ${value}`,
+  //     },
+  //   })
+  //     .then((response) => {
+  //       const { data } = response;
+  //       const { msg } = data;
+  //       // document.getElementById("name").innerHTML = msg ? `${msg}` : "No message available";
+  //     })
+  //     .catch((error) => {
+  //       console.error("Axios error:", error);
+  //     });
+  // };
 
-  useEffect(() => {
-    GetName();
-  }, []);
+  // useEffect(() => {
+  //   GetName();
+  // }, []);
 
 
     const [username, setUsername] = useState("");
